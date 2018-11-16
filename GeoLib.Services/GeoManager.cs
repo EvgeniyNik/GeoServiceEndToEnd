2276560de69e5a6f374a60ac6b806b0ba670355e
EvgeniyNik <darth77@yandex.ru>
2018-11-16 22:37:45 +0200

    add GeoManager service declaration

---
+++ GeoLib.Services/GeoManager.cs
@@ -0,0 +1,29 @@
+﻿using GeoLib.Contracts;
+using System;
+using System.Collections.Generic;
+
+namespace GeoLib.Services
+{
+    public class GeoManager : IGeoService
+    {
+        public IEnumerable<string> GetStates(bool primaryOnly)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ZipCodeData GetZipInfo(string zip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ZipCodeData> GetZips(string state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ZipCodeData> GetZips(string zip, int range)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
