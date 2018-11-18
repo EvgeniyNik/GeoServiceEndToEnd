321c727c283a121d0c786f05cf3fd527baee5284
EvgeniyNik <darth77@yandex.ru>
2018-11-18 21:03:40 +0200

    add test project with test method

---
+++ GeoLib.Services.Tests/GeoManagerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeoLib.Contracts;
+using GeoLib.Data;
+using NSubstitute;
+using Xunit;
+
+namespace GeoLib.Services.Tests
+{
+    public class GeoManagerTests
+    {
+        [Fact]
+        public void TestZipCodeRetrieval()
+        {
+            IZipCodeRepository repository = Substitute.For<IZipCodeRepository>();
+            ZipCode zipCode = new ZipCode()
+            {
+                City = "LINCOLN PARK",
+                State = new State() { Abbreviation = "NJ"},
+                Zip = "07035"
+            };
+            repository.GetByZip("07035").Returns(zipCode);
+            IGeoService geoManager = new GeoManager(repository);
+
+            var data = geoManager.GetZipInfo(zipCode.Zip);
+
+            Assert.Equal(zipCode.City, data.City);
+            Assert.Equal(zipCode.State.Abbreviation, data.State);
+        }
+    }
+}
